378b0eeb2246d9f05afab9716958cb686edfd8e0
Jin <ktj9279@gmail.com>
2017-01-18 00:18:50 +0900

    hello.py modified on local repository

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-print("Hello world")
+// For command line git tutorial
+// Local repository modification
+print("Hellio world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
